ab9e3bb64aecccd940c8f6825927cd8d9e9500b5
644659002@qq.COM <eZb-KJe-tdu-B7M>
2015-03-22 09:54:52 +0800

    some enters

--- app/src/main/java/com/example/pcuser/myweather/MainActivity.java
+++ app/src/main/java/com/example/pcuser/myweather/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends ActionBarActivity {
 
 
     @Override
+    protected void onStart(){
+        
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
